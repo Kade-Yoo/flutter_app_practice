9d31ad2aac19a64f96499e123ccf819eb4026bf5
Kade <yhm9207@naver.com>
2023-09-25 13:09:43 +0900

    android app 기본 ui 테스트
    - image, space, padding 추가

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,51 @@ class StorePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Text(
-                "Women",
-                style: TextStyle(fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Text(
+                      "Women",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),   // 공간을 넓히는 Object
+                  Text(
+                    "Kids",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    "Shoes",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Spacer(),
+                  Text(
+                    "Bag",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ]
               ),
-              Text(
-                "Kids",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Shoes",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "Bag",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ]
-          )
-        ]
+            ),
+            Expanded(
+                flex: 1,
+                child: Image.asset("assets/bag.jpeg",
+                    fit: BoxFit.cover)
+            ), // BoxFit.cover 크기에 맞춰 넓힘
+            const SizedBox(
+              height: 2,
+            ),
+            Expanded(
+                flex: 1,
+                child: Image.asset("assets/cloth.jpeg",
+                    fit: BoxFit.cover)
+            )
+          ]
+        ),
       ),
     );
   }
